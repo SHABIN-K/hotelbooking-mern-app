debe6c247fdd6a526786a573ff52c9de2af019e3
Shabin-k <helloshabink@gmail.com>
2024-02-03 16:10:01 +0530

    feat : add header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+    </Routes>
   );
 }
 
