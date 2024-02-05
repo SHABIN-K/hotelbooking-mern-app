5be2366f90e9ac39d5afe12aeaa418db2edce764
Shabin-k <helloshabink@gmail.com>
2024-02-05 19:52:32 +0530

    e

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.post("/register", async (req: Request, res: Response) => {});
+router.post("/register", async (req: Request, res: Response) => {
+    console.log("hello register")
+});
 
 export default router;
