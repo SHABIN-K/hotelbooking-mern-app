de4a2a31f2771fd100ac9a9edd036ee10a298ef1
Shabin-k <helloshabink@gmail.com>
2024-03-02 21:17:20 +0530

    feat : add unit test (playwright)

---
+++ e2e/tests/auth.spec.ts
@@ -0,0 +1,52 @@
+import { test, expect } from "@playwright/test";
+
+const UI_URL = "http://localhost:3000";
+
+test("should allow the user to sign in", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  //get the sign in button
+  await page.getByRole("link", { name: "Sign In" }).click();
+
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("me@1.com");
+  await page.locator("[name=password]").fill("123456");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Sign in Successful!")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
+
+test("should allow the user to Register", async ({ page }) => {
+  const testEmail = `test_register_${
+    Math.floor(Math.random() * 90000) + 10000
+  }@test.com`;
+
+  await page.goto(UI_URL);
+
+  //get the sign in button
+  await page.getByRole("link", { name: "Sign In" }).click();
+  await page.getByRole("link", { name: "Create an account here" }).click();
+
+  await expect(
+    page.getByRole("heading", { name: "Create an Account" })
+  ).toBeVisible();
+
+  //fill the credentials
+  await page.locator("[name=firstName]").fill("test_firstName");
+  await page.locator("[name=lastName]").fill("test_lastName");
+  await page.locator("[name=email]").fill(testEmail);
+  await page.locator("[name=password]").fill("password123");
+  await page.locator("[name=confirmPassword]").fill("password123");
+
+  await page.getByRole("button", { name: "Create Account" }).click();
+
+  await expect(page.getByText("Registration Success!")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
