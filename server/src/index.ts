9c694abee3f0179ca079af35bfd395e7880c6668
Shabin-k <helloshabink@gmail.com>
2024-03-02 22:26:52 +0530

    feat : add cloud storage

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,17 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from 'cloudinary';
 
 import connectDB from "./database/connect";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
+          
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(cookieParser());
