bc5dc539cfb10e6b7d4ea4fc36f1784e85352bc2
Shabin-k <helloshabink@gmail.com>
2024-04-13 10:35:14 +0530

    feat : add search bar

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,6 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./Hero";
+import SearchBar from "./SearchBar";
 
-export { Hero, Header, Footer };
+export { Hero, Header, Footer, SearchBar };
--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Footer, Header, Hero } from "../components";
+import { Footer, Header, Hero, SearchBar } from "../components";
 
 interface Props {
   children: React.ReactNode;
@@ -10,6 +10,9 @@ const Layout = ({ children }: Props) => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <Hero />
+      <div className="container mx-auto">
+        <SearchBar />
+      </div>
       <div className="container mx-auto py-10 flex-1">{children}</div>
       <Footer />
     </div>
